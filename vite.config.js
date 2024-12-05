619d61d82493cbc1c68931f4ebb7f6b14be5e612
Your Name <srikanthgoud783813@gmail.com>
2024-12-05 08:59:33 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Srikanthraparthi/MovieApp', // Replace with your GitHub repo name
   plugins: [react()],
-})
+});
