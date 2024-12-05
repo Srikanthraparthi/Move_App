6aff398dba6d320c35d2f2e46788de9b76330e48
srikanth <126452337+Srikanthraparthi@users.noreply.github.com>
2024-12-05 09:26:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Import the App component
+import './index.css'; // Import any global CSS you might have
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root')); // Get the root element
+root.render(
+  <React.StrictMode>
+    <App /> {/* Render the App component */}
+  </React.StrictMode>
+);
